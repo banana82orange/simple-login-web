ac922dd59b32cb5cc07063b5569ce71e8af268e9
Jirawat <jirawat.kri@ku.th>
2025-10-14 00:19:09 +0700

    Update HomePage.jsx

--- frontEnd/login-page/src/pages/Home/HomePage.jsx
+++ frontEnd/login-page/src/pages/Home/HomePage.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+          <div>HomePage</div>
+          <div>HomePage2</div>
+          <div>HomePage3</div>
+          <div>HomePag4</div>
+    </div>
   )
 }
